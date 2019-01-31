d6b56670092948ee915f4b09baf616e2497e8f1f
Alex LaFroscia <alex@lafroscia.com>
2019-01-31 14:36:44 -0800

    add ember-cli-deploy

--- config/environment.js
+++ config/environment.js
@@ -48,7 +48,8 @@ module.exports = function(environment) {
   }
 
   if (environment === "production") {
-    // here you can enable a production-specific feature
+    ENV.rootURL = "/____apollo-watch-query-subscription-bug";
+    ENV.locationType = "hash";
   }
 
   return ENV;
